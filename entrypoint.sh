108921c4cac81030f6f4adfd3edf20a14f939f4e
Robert Ullrich <roul76@googlemail.com>
2020-03-23 20:16:33 +0100

    code style, more output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,29 @@
-#!/bin/sh
-set -o pipefail
+#!/bin/ash
+set -eo pipefail
 
+echo "*** Start initialization ***"
 # Create login user
-[ "${WEBCONSOLE_USER}" != "" -a "${WEBCONSOLE_HASH}" != "" -a "${WEBCONSOLE_SHELL}" != "" ] && \
-  adduser -h /home/"${WEBCONSOLE_USER}" -s "${WEBCONSOLE_SHELL}" "${WEBCONSOLE_USER}" && \
-  echo "${WEBCONSOLE_USER}:${WEBCONSOLE_HASH}" | chpasswd -e && \
-  sshdir=/home/"${WEBCONSOLE_USER}"/.ssh && \
-  mkdir -p "${sshdir}" && chmod 700 "${sshdir}" && chown "${WEBCONSOLE_USER}" "${sshdir}" && \
+if [ "${WEBCONSOLE_USER}" != "" ] && \
+   [ "${WEBCONSOLE_HASH}" != "" ] && \
+   [ "${WEBCONSOLE_SHELL}" != "" ]; then
+  echo "Adding user '${WEBCONSOLE_USER}' with shell '${WEBCONSOLE_SHELL}'"
+  adduser -D -h "/home/${WEBCONSOLE_USER}" -s "${WEBCONSOLE_SHELL}" "${WEBCONSOLE_USER}"
+
+  echo "Changing password for user '${WEBCONSOLE_USER}'"
+  echo "${WEBCONSOLE_USER}:${WEBCONSOLE_HASH}"|chpasswd -e
+
+  sshdir="/home/${WEBCONSOLE_USER}/.ssh"
+  echo "Creating directory ${sshdir}"
+  mkdir -p "${sshdir}"
+  chmod 700 "${sshdir}"
+  chown "${WEBCONSOLE_USER}" "${sshdir}"
+fi
+
 
 # Enable auto update of /etc/hosts by permanently
 # looking for /webconsole/*.hosts
 if [ -r /webconsole/ ]; then
+  echo "Starting hostkey-watcher"
   cp /etc/hosts /etc/hosts.backup
   (
     while : ; do
@@ -26,4 +39,5 @@ fi
 unset WEBCONSOLE_USER
 unset WEBCONSOLE_HASH
 
+echo "*** Finished initialization ***"
 exec "$@"
